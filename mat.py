486d026ebc9d468edc4ae56cb52c85a36b34c8bc
EnukNogueira <enuk.santos@gmail.com>
2026-05-14 23:06:02 -0300

    Adicionando a Biblioteca Matplotlib

--- mat.py
+++ mat.py
@@ -1,5 +1,6 @@
 import random
 import json
+import matplotlib.pyplot as plt
 
 name = input("Digite seu nome: ")
 
@@ -21,6 +22,7 @@ while True:
     else:
         print("Resposta errada, tente novamente!")
 
+
     """Convertendo os Dados para Json"""
 
     data_for_round = {
@@ -29,7 +31,6 @@ while True:
         f"{name}": response_user,
         "resposta_correta": number_1 + number_2
     }
-
     try:
         with open('resultados.json', 'r', encoding='utf-8') as arquivo:
             historico = json.load(arquivo)
@@ -46,3 +47,36 @@ while True:
     cont = input("Deseja continuar jogando? (s/n): ").strip().lower()
     if cont not in ("s", "sim", "y", "yes"):
         break
+
+print("\nFim do Jogo")
+print("Fazendo o gráfico dos jogadores...")
+
+acertos = 0
+erros = 0
+
+
+for rodada in historico:
+    if rodada.get("nome do jogador") == name:
+        resposta_usuario = rodada.get(name)
+        resposta_correta = rodada.get("resposta_correta")
+        
+        if resposta_usuario == resposta_correta:
+            acertos += 1
+        else:
+            erros += 1
+
+# Teste de biblioteca Matplotlib
+categorias = ['Acertos', 'Erros']
+quantidades = [acertos, erros]
+cores = ['#4CAF50', '#F44336'] #Igual no minecraft quando criava uma textura.
+
+plt.bar(categorias, quantidades, color=cores)
+plt.title(f'Resultado de {name}')
+plt.xlabel('Resultados')
+plt.ylabel('Quantidade de Partidas')
+
+import math
+plt.yticks(range(0, max(quantidades) + 2))
+
+#Imprime o gráfico
+plt.show()
